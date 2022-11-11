fda19d7eac3e2862346a40ae218b57e79d1db880
nabil <pni20156789@gmail.com>
2022-11-11 13:37:07 +0000

    create general types for production-chain & site

---
+++ src/core/types/types.ts
@@ -0,0 +1,111 @@
+
+
+export interface ISite {
+    id: string,
+    user: IUser,
+    template: ITemplate,
+    products: IProduct[],
+}
+
+export interface IUser {
+    id: string,
+    name: string,
+    email: string,
+    groups: IGroup[]
+}
+
+export interface IGroup {
+    id: string,
+    site: ISite,
+    name: string,
+    users: IUser[],
+    tag: ITag[],
+
+}
+
+export interface ITag {
+    id: string,
+    name: string,
+}
+
+export interface ITemplate {
+    id: string,
+    name: string,
+    description: string,
+}
+
+export interface IProduct {
+    id: string,
+    metadata: IProductMetadata,
+}
+
+export interface ICustomProduct extends IProduct {
+    isCustom: true,
+    priorities: ICustomProductPriority[],
+    form: ICustomProductForm,
+}
+
+export interface ICustomProductForm {
+    id: string,
+    version: number,
+    lastUpdated: Date,
+    fields: ICustomProductFormField[],
+}
+
+export interface ICustomProductFormField {
+    id: string,
+    name: string,
+    type: "text" | "number" | "date" | "select" | "checkbox" | "radio" | "textarea",
+    required: boolean,
+    options: string[],
+}
+
+export interface ICustomProductPriority {
+    id: string,
+    name: string,
+    description: string,
+    fieldId: string,
+    priority : number,
+}
+
+
+
+
+
+export interface IProductMetadata {
+    name: string,
+    description: string,
+}
+
+export interface IOrder {
+    id: string,
+    user: IUser,
+    site: ISite,
+    product: IProduct,
+    line: ILine,
+    status: "pending" | "processing" | "completed" | "cancelled" | "expired",
+    price: number,
+}
+
+export interface ILine {
+    id: string,
+    name: string,
+    groups: IGroup[],
+    maxOrders: number,
+    isPublic: boolean,
+    next?: ILine,
+    expiresTime: number,
+    confirmations: IConfirmation[],
+}
+
+export interface IConfirmation {
+    id: string,
+    type: "email" | "sms" | "phone" | "QR" | "payment" | "verification",
+    group?: IGroup,
+    user?: IUser,
+    line: ILine,
+    order: IOrder,
+}
+
+
+
