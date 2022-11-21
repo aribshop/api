87d4f2b5fb2849299cb7f01b4e4d526ca2653c74
nabildroid <pni20156789@gmail.com>
2022-11-21 20:47:35 +0100

    more explicit product type

--- src/domains/sites/repositories/product.ts
+++ src/domains/sites/repositories/product.ts
@@ -1,65 +1,71 @@
-import { ICustomProductModel, IProductEntity, IProductModel, IStandardProductModel } from "../types/product";
+import {
+  ICustomProductModel,
+  IProductEntity,
+  IProductModel,
+  IStandardProductModel,
+} from "../types/product";
 import { delay } from "../../../core/util";
 
 export async function getProducts(siteId: string): Promise<IProductModel[]> {
+  await delay(1000);
 
-    await delay(1000);
-
-    // todo for testing purposes, return only one kind of product
-    const custom: ICustomProductModel = {
-        id: siteId,
-        metadata: {
-            name: "My custom product",
-            description: "My custom product description",
-            tag: ["tag1", "tag2"],
+  // todo for testing purposes, return only one kind of product
+  const custom: ICustomProductModel = {
+    id: siteId,
+    isPaused: false,
+    metadata: {
+      name: "My custom product",
+      description: "My custom product description",
+      tag: ["tag1", "Referral"],
+    },
+    isCustom: true,
+    dailyLimit: 10,
+    form: {
+      id: "sdsd",
+      version: 1,
+      lastUpdated: new Date(),
+      fields: [
+        {
+          id: "sdsd",
+          name: "My field",
+          type: "text",
+          required: true,
+          options: [],
         },
-        isCustom: true,
-        form: {
-            id: "sdsd",
-            version: 1,
-            lastUpdated: new Date(),
-            fields: [
-                {
-                    id: "sdsd",
-                    name: "My field",
-                    type: "text",
-                    required: true,
-                    options: [],
-                }
-            ]
-        }
-    }
+      ],
+    },
+  };
 
-    const standard: IStandardProductModel = {
-        id: "sdsd",
-        metadata: {
-            name: "My standard product",
-            description: "My standard product description",
-            tag: ["tag1", "tag2"],
-        },
-        isCustom: false,
-        price: 100,
-        quantity: 10,
-        discount: 0,
-        picture: "https://laknabil.me/background.png",
-    }
+  const standard: IStandardProductModel = {
+    id: "sdsd",
+    metadata: {
+      name: "My standard product",
+      description: "My standard product description",
+      tag: ["A/B", "tag2"],
+    },
+    isCustom: false,
+    price: 100,
+    isPaused: false,
+    quantity: 10,
+    discount: 0,
+    picture: "https://laknabil.me/background.png",
+  };
 
-
-    return [custom, standard];
-
+  return [custom, standard];
 }
-
 
 export async function getProduct(productId: string): Promise<IProductModel> {
-    return (await getProducts(productId))[Math.random() > .5 ? 1 : 0];
+  return (await getProducts(productId))[Math.random() > 0.5 ? 1 : 0];
 }
 
-
-export async function createProduct(product: IProductEntity,siteId:string): Promise<IProductModel> {
-    await delay(1000);
-    // todo test if the product is valid, specially the custom product
-    return {
-        id: siteId,
-        ...product
-    }
+export async function createProduct(
+  product: IProductEntity,
+  siteId: string
+): Promise<IProductModel> {
+  await delay(1000);
+  // todo test if the product is valid, specially the custom product
+  return {
+    id: siteId,
+    ...product,
+  };
 }
--- src/domains/sites/types/product.ts
+++ src/domains/sites/types/product.ts
@@ -1,75 +1,80 @@
 import { IProductMetadata } from "../../../core/types/types";
 
 export interface IProduct {
-    id: string,
-    metadata: IProductMetadata,
-    isCustom: boolean,
+  id: string;
+  metadata: IProductMetadata;
+  isCustom: boolean;
 }
-
-
-
 
 export interface IProductModel extends IProductEntity {
-    id: string,
+  id: string;
 }
 export interface IProductEntity {
-    metadata: IProductMetadataEntity,
-    isCustom: boolean,
+  metadata: IProductMetadataEntity;
+  isCustom: boolean;
+  isPaused: boolean;
 }
 
 export interface IProductMetadataEntity {
-    name: string,
-    description: string,
-    tag: string[],
+  name: string;
+  description: string;
+  tag: string[];
 }
 
 export interface IStandardProductEntity extends IProductEntity {
-    isCustom: false,
-    price: number,
-    quantity: number,
-    discount: number,
-    picture: string,
+  isCustom: false;
+  price: number;
+  quantity: number;
+  discount: number;
+  picture: string;
 }
 
 // FIXME don't use this pattern, duplicate code is way better than this
-export interface IStandardProductModel extends IProductModel, IStandardProductEntity {
-    isCustom: false,
+export interface IStandardProductModel
+  extends IProductModel,
+    IStandardProductEntity {
+  isCustom: false;
 }
-
-
 
 export interface ICustomProductEntity extends IProductEntity {
-    isCustom: true,
-    form: ICustomProductFormEntity,
+  isCustom: true;
+  form: ICustomProductFormEntity;
 }
 
-export interface ICustomProductModel extends IProductModel {
-    isCustom: true,
-    form: ICustomProductFormModel,
+export interface ICustomProductModel
+  extends ICustomProductEntity,
+    IProductModel {
+  isCustom: true;
+  form: ICustomProductFormModel;
+  dailyLimit: number; // todo this should enforced by the first line in the chain | the reason for it to be a model, is this is a calculated value! (aggregated)
 }
-
-
 
 export interface ICustomProductFormEntity {
-    version: number,
-    lastUpdated: Date,
-    fields: ICustomProductFormFieldEntity[],
+  version: number;
+  lastUpdated: Date;
+  fields: ICustomProductFormFieldEntity[];
 }
 
 export interface ICustomProductFormModel extends ICustomProductFormEntity {
-    id: string,
-    fields: ICustomProductFormFieldModel[],
-
+  id: string;
+  fields: ICustomProductFormFieldModel[];
 }
 
 export interface ICustomProductFormFieldEntity {
-    name: string,
-    type: "text" | "number" | "date" | "select" | "checkbox" | "radio" | "textarea",
-    required: boolean,
-    options: string[],
+  name: string;
+  type:
+    | "text"
+    | "number"
+    | "date"
+    | "select"
+    | "checkbox"
+    | "radio"
+    | "textarea";
+  required: boolean;
+  options: string[];
 }
 
-export interface ICustomProductFormFieldModel extends ICustomProductFormFieldEntity {
-    id: string,
-
+export interface ICustomProductFormFieldModel
+  extends ICustomProductFormFieldEntity {
+  id: string;
 }
